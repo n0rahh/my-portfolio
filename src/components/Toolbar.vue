8fb9d7eda5477b61052e0b316834dbfe21515ce3
Vlad <vlad.herasymovych@tutore.eu>
2023-02-12 22:21:23 +0100

    feat: move theme toggle into separate component

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -17,10 +17,7 @@
         </div>
         <v-spacer />
         <div class="options d-flex">
-          <div class="options-switch d-flex align-center">
-            <input type="checkbox" id="toggle">
-            <label for="toggle" class="button"></label>
-          </div>
+          <theme-toggle />
           <div class="options-lang ml-4 d-flex align-center">
             <v-menu 
               v-model="open"
@@ -51,8 +48,13 @@
 </template>
 
 <script>
+import ThemeToggle from './toolbar/ThemeToggle.vue';
+
 export default {
   name: 'Toolbar',
+  components: {
+    ThemeToggle,
+  },
   data() {
     return {
       currentLang: 'EN',
@@ -92,45 +94,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#toggle {
-  display: none;
-}
-
-.button {
-  display: inline-block;
-  width: 60px;
-  height: 25px;
-  background-color: #fff;
-  border-radius: 30px;
-  position: relative;
-  cursor: pointer;
-  &::after {
-    content: url('../assets/icons/sun.svg');
-    width: 30px;
-    height: 30px;
-    background-color: #e03c3c;
-    border: 2px solid #fff;
-    border-radius: 50%;
-    box-shadow: 0 0 5px rgba(0, 0, 0, .25);
-    position: absolute;
-    top: -2.5px;
-    left: 0;
-    display: grid;
-    place-content: center;
-    line-height: 0;
-    transition: 1s;
-    transform: 1s ease-in;
-  }
-}
-
-#toggle:checked + .button {
-  &::after {
-    content: url('../assets/icons/moon.svg');
-    background-color: #28309b;
-    transform: translateX(30px) rotate(360deg);
-  }
-}
-
 .lang-btn {
   cursor: pointer;
 }
---
+++ src/components/toolbar/ThemeToggle.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="options-switch d-flex align-center">
+    <input type="checkbox" id="toggle">
+    <label for="toggle" class="button"></label>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#toggle {
+  display: none;
+}
+
+.button {
+  display: inline-block;
+  width: 60px;
+  height: 25px;
+  background-color: #fff;
+  border-radius: 30px;
+  position: relative;
+  cursor: pointer;
+  &::after {
+    content: url('../../assets/icons/sun.svg');
+    width: 30px;
+    height: 30px;
+    background-color: #e03c3c;
+    border: 2px solid #fff;
+    border-radius: 50%;
+    box-shadow: 0 0 5px rgba(0, 0, 0, .25);
+    position: absolute;
+    top: -2.5px;
+    left: 0;
+    display: grid;
+    place-content: center;
+    line-height: 0;
+    transition: 1s;
+    transform: 1s ease-in;
+  }
+}
+
+#toggle:checked + .button {
+  &::after {
+    content: url('../../assets/icons/moon.svg');
+    background-color: #28309b;
+    transform: translateX(30px) rotate(360deg);
+  }
+}
+</style>
