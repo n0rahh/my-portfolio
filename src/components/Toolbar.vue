58e933ad98704c8a54e77999dc71694b0a2b4e39
Vlad <vlad.herasymovych@tutore.eu>
2023-02-12 22:27:25 +0100

    feat: move language switch into separate component

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -18,30 +18,7 @@
         <v-spacer />
         <div class="options d-flex">
           <theme-toggle />
-          <div class="options-lang ml-4 d-flex align-center">
-            <v-menu 
-              v-model="open"
-              offset-y
-            >
-              <template v-slot:activator="{ props }">
-                <div
-                  v-bind="props"
-                  class="lang-btn"
-                >
-                {{ currentLang }} {{ open ? '▴' : '▾' }}
-                </div>
-              </template>
-              <v-list class="pa-0 mt-2">
-                <v-list-item
-                  v-for="(lang, i) in langs"
-                  :key="`l${i}`"
-                  @click="changeLang(lang)"
-                >
-                  {{ lang }}
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </div>
+          <lang-switch />
         </div>
       </v-container>
   </v-app-bar>
@@ -49,16 +26,16 @@
 
 <script>
 import ThemeToggle from './toolbar/ThemeToggle.vue';
+import LangSwitch from './toolbar/LangSwitch.vue';
 
 export default {
   name: 'Toolbar',
   components: {
     ThemeToggle,
+    LangSwitch,
   },
   data() {
     return {
-      currentLang: 'EN',
-      open: false,
       navigationList: [
         this.$t('TOOLBAR.NAVIGATION.MAIN_PAGE'),
         this.$t('TOOLBAR.NAVIGATION.PROJECT'),
@@ -66,35 +43,7 @@ export default {
         this.$t('TOOLBAR.NAVIGATION.ABOUT'),
         this.$t('TOOLBAR.NAVIGATION.CONTACT'),
       ],
-      langs: [
-        this.$t('TOOLBAR.LANG.LONG_LANGS.ENG'),
-        this.$t('TOOLBAR.LANG.LONG_LANGS.POL'),
-        this.$t('TOOLBAR.LANG.LONG_LANGS.UKR'),
-      ],
     };
-  },
-  methods: {
-    changeLang(lang) {
-      switch(lang) {
-      case 'English':
-        this.currentLang = this.$t('TOOLBAR.LANG.SHORT_LANGS.ENG');
-        break;
-      case 'Polski':
-        this.currentLang = this.$t('TOOLBAR.LANG.SHORT_LANGS.POL');
-        break;
-      case 'Українська':
-        this.currentLang = this.$t('TOOLBAR.LANG.SHORT_LANGS.UKR');
-        break;
-      default:
-        this.currentLang = this.$t('TOOLBAR.LANG.SHORT_LANGS.ENG');
-      }
-    },
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.lang-btn {
-  cursor: pointer;
-}
-</style>
---
+++ src/components/toolbar/LangSwitch.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="options-lang ml-4 d-flex align-center">
+    <v-menu 
+        v-model="open"
+        offset-y
+    >
+        <template v-slot:activator="{ props }">
+        <div
+            v-bind="props"
+            class="lang-btn"
+        >
+        {{ currentLang }} {{ open ? '▴' : '▾' }}
+        </div>
+        </template>
+        <v-list class="pa-0 mt-2">
+        <v-list-item
+            v-for="(lang, i) in langs"
+            :key="`l${i}`"
+            @click="changeLang(lang)"
+        >
+            {{ lang }}
+        </v-list-item>
+        </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentLang: 'EN',
+      open: false,
+      langs: [
+        this.$t('TOOLBAR.LANG.LONG_LANGS.ENG'),
+        this.$t('TOOLBAR.LANG.LONG_LANGS.POL'),
+        this.$t('TOOLBAR.LANG.LONG_LANGS.UKR'),
+      ],
+    };
+  },
+  methods: {
+    changeLang(lang) {
+      switch(lang) {
+      case 'English':
+        this.currentLang = this.$t('TOOLBAR.LANG.SHORT_LANGS.ENG');
+        break;
+      case 'Polski':
+        this.currentLang = this.$t('TOOLBAR.LANG.SHORT_LANGS.POL');
+        break;
+      case 'Українська':
+        this.currentLang = this.$t('TOOLBAR.LANG.SHORT_LANGS.UKR');
+        break;
+      default:
+        this.currentLang = this.$t('TOOLBAR.LANG.SHORT_LANGS.ENG');
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.lang-btn {
+  cursor: pointer;
+}
+</style>
