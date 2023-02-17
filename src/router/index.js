53eefcbd33d9b64ca2c4acb9bdae191e4525ed9e
Vlad <vlad.herasymovych@tutore.eu>
2023-02-17 13:34:07 +0100

    feat: add new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '../views/Home.vue';
+import Projects from '../views/Projects.vue';
+import Skills from '../views/Skills.vue';
+import About from '../views/About.vue';
+import Contact from '../views/Contact.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -10,7 +14,26 @@ const router = createRouter({
       name: 'Home',
       component: Home,
     },
-   
+    { 
+      path: '/projects',
+      name: 'Projects',
+      component: Projects,
+    },
+    { 
+      path: '/skills',
+      name: 'Skills',
+      component: Skills,
+    },
+    { 
+      path: '/about',
+      name: 'About',
+      component: About,
+    },
+    { 
+      path: '/contact',
+      name: 'Contact',
+      component: Contact,
+    },
   ],
 });
 export default router;
