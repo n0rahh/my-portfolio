c1aaeb061a161f5021852ed0586a209a21f3187a
Vlad <vlad.herasymovych@tutore.eu>
2023-02-17 13:31:56 +0100

    fix: change messages in i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,20 @@
 import { createI18n } from 'vue-i18n';
-import pl from './locales/pl.json';
-import en from './locales/en.json';
-import ua from './locales/ua.json';
+
+function loadLocaleMessages() {
+  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
+  const messages = {};
+  locales.keys().forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+    if (matched && matched.length > 1) {
+      const locale = matched[1];
+      messages[locale] = locales(key).default;
+    }
+  });
+  return messages;
+}
 
 export default createI18n({
   locale: 'en',
   fallbackLocale: 'en',
-  messages: { pl, en, ua },
+  messages: loadLocaleMessages(),
 });
