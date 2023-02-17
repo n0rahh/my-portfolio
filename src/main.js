e0647c85f434d2ad67a164f9e2dbdf4c3a32141b
Vlad <vlad.herasymovych@tutore.eu>
2023-02-17 13:30:49 +0100

    fix: change main.js file

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
 import i18n from './i18n';
+import router from './router/index';
+import store from './store/index';
 
 loadFonts();
 
-createApp(App).use(i18n)
-  .use(vuetify)
-  .mount('#app');
+const app = createApp(App);
+app.use(i18n);
+app.use(router);
+app.use(store);
+app.use(vuetify);
+
+app.mount('#app');
