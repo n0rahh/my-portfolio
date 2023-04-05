f7463078a50bfcbb2291b053d9253fba2c62be7a
Vlad <vlad.herasymovych@tutore.eu>
2023-04-05 19:27:10 +0200

    feat: add skills section

---
+++ src/components/home/Skills.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container class="container py-16">
+    <v-row>
+      <v-col
+        cols="5"
+        class="d-flex flex-column align-start"
+      >
+        <span class="h0 my-8">{{ $t('HOME_PAGE.HOME_SKILLS.SKILLS_TITLE') }}</span>
+
+        <main-skills />
+      </v-col>
+      <v-col
+        cols="2"
+        class="d-flex justify-center"
+      >
+        <span class="h0 my-8">&amp;</span>
+      </v-col>
+
+      <v-col
+        cols="5"
+        class="d-flex justify-end"
+      >
+        <span class="h0 my-8">{{ $t('HOME_PAGE.HOME_SKILLS.WORK_TITLE') }}</span>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import MainSkills from '@/components/skills/MainSkills.vue';
+
+export default {
+  name: 'Skills',
+  components: {
+    MainSkills,
+  },
+  
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100vh;
+}
+</style>
---
+++ src/components/skills/MainSkills.vue
@@ -0,0 +1,126 @@
+<template>
+  <v-container fluid>
+    <v-row
+      v-for="(skill, index) in skills"
+      :key="index"
+    >
+      <v-col
+        cols="4"
+        class="d-flex align-center"
+      >
+        <span class="h4">{{ skill.name }}</span>
+      </v-col>
+      <v-col
+        cols="8"
+        class="d-flex align-center"
+      >
+        <div class="progress-wrapper">
+          <div
+            class="progress-wrapper_bar w-600 d-flex justify-end pr-2"
+            :class="{ 
+              'fill': animate 
+            }"
+            :style="progressBarWidth(skill.percent)"
+          >
+            {{ skill.percent }}
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'MainSkills',
+  data() {
+    return {
+      animate: false,
+      progress: 0,
+      skills: [
+        {
+          name: 'Vue.js',
+          percent: '40%',
+        },
+        {
+          name: 'Node.js',
+          percent: '20%',
+        },
+        {
+          name: 'JavaScript',
+          percent: '40%',
+        },
+        {
+          name: 'TypeScript',
+          percent: '20%',
+        },
+        {
+          name: 'Express',
+          percent: '60%',
+        },
+        {
+          name: 'Nest',
+          percent: '20%',
+        },
+        {
+          name: 'REST API',
+          percent: '50%',
+        },
+        {
+          name: 'Cypress',
+          percent: '30%',
+        },
+        {
+          name: 'SCRUM',
+          percent: '50%',
+        },
+        {
+          name: 'Jira',
+          percent: '40%',
+        },
+      ],
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.animate = true;
+    }, 100);
+  },
+  methods: {
+    progressBarWidth(index) {
+      return `--progress: ${index}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/colors.scss';
+
+.progress-wrapper {
+  width: 100%;
+  height: 30px;
+  border-radius: 10px;
+  border: 2px solid $dark-green;
+  &_bar {
+    height: 100%;
+    width: 0%;
+    background-color: $light-green;
+    border-radius: 9px 0 0 9px;
+    color: $black;
+  }
+}
+
+.fill {
+  animation: fill-progress 1s forwards;
+}
+
+@keyframes fill-progress {
+  0% {
+    width: 0%;
+  }
+  100% {
+    width: var(--progress);
+  }
+}
+</style>
