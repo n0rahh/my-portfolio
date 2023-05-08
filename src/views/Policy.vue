86815493b8c5913eb5a902698b549dd2bc63162d
Vlad <vlad.herasymovych@tutore.eu>
2023-05-08 19:06:22 +0200

    feat: add translations for policy

--- src/views/Policy.vue
+++ src/views/Policy.vue
@@ -2,45 +2,63 @@
   <v-container class="container py-16">
     <v-row>
       <v-col cols="12">
-        <span class="h2">Polityka prywatności</span>
-        <p class="p2 mt-16">
-          Niniejsza polityka prywatności określa zasady przetwarzania i ochrony danych osobowych przekazywanych przez użytkowników za pośrednictwem formularza kontaktowego na stronie portfolio [nazwa]. 
-          <br><br>
-          Administratorem danych osobowych przetwarzanych w ramach strony portfolio jest Vlad Herasymovych.
-          <br><br>
-          1. Rodzaje przetwarzanych danych osobowych oraz cel ich przetwarzania
-          <br><br>
-          W ramach formularza kontaktowego przetwarzane są następujące dane osobowe:
-          <br><br>
-          • imię i nazwisko,
-          • adres e-mail,
-          • treść wiadomości,
-          • plik (opcjonalnie).
-          <br>
-          Dane te są przetwarzane w celu umożliwienia kontaktu z użytkownikami strony portfolio w odpowiedzi na ich pytania i prośby. W przypadku przesyłania pliku, dane te są przetwarzane w celu umożliwienia przekazania użytkownikom pliku.
-          <br><br>
-          2. Podstawa prawna przetwarzania danych osobowych
-          <br><br>
-          Podstawą prawną przetwarzania danych osobowych jest art. 6 ust. 1 lit. f RODO, czyli prawnie uzasadniony interes administratora, którym jest udzielanie odpowiedzi na pytania i prośby użytkowników strony portfolio.
-          <br><br>
-          3. Okres przetwarzania danych osobowych
-          <br><br>
-          Dane osobowe przetwarzane w ramach formularza kontaktowego będą przechowywane przez okres niezbędny do udzielenia odpowiedzi na pytanie lub prośbę użytkownika, a także do ewentualnego późniejszego kontaktu w związku z danym zapytaniem. Po tym czasie dane zostaną usunięte.
-          <br><br>
-          4. Odbiorcy danych osobowych
-          <br><br>
-          Dane osobowe użytkowników strony portfolio nie są udostępniane żadnym innym podmiotom, z wyjątkiem sytuacji, gdy jest to wymagane przez przepisy prawa.
-          5. Pliki cookie i inne technologie
-          <br><br>
-          Strona portfolio nie wykorzystuje plików cookie ani innych technologii śledzących. Jedynie przechowuje wartość locale w localStorage w celu zapamiętania wybranego przez użytkownika języka strony. Ta informacja jest wykorzystywana wyłącznie do poprawnego wyświetlania strony w wybranym języku i nie jest udostępniana innym podmiotom. Użytkownicy strony mogą w każdej chwili usunąć tę wartość z localStorage poprzez ustawienie języka na domyślny lub wyczyść localStorage całkowicie.
-        </p>
+        <span class="h2">{{ $t('POLICY.TITLE') }}</span>
+        <br><br>
+        <span class="p2 mt-16">{{ $t('POLICY.DESCRIPTION') }}</span>
+        <div class="p2 mt-8">
+          <p
+            v-for="(pkt, index) in policyPkts"
+            :key="index"
+          >
+            <span>{{ pkt.title }}</span>
+            <br>
+            <span class="formatted-text">{{ formattedText(pkt.description) }}</span>
+          </p>
+        </div>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
+<script>
+export default {
+  name: 'Policy',
+  computed: {
+    policyPkts() {
+      return [
+        {
+          title: this.$t('POLICY.PKTS.TITLE_1'),
+          description: this.$t('POLICY.PKTS.DESCRIPTION_1'),
+        },
+        {
+          title: this.$t('POLICY.PKTS.TITLE_2'),
+          description: this.$t('POLICY.PKTS.DESCRIPTION_2'),
+        },
+        {
+          title: this.$t('POLICY.PKTS.TITLE_3'),
+          description: this.$t('POLICY.PKTS.DESCRIPTION_3'),
+        },
+        {
+          title: this.$t('POLICY.PKTS.TITLE_4'),
+          description: this.$t('POLICY.PKTS.DESCRIPTION_4'),
+        },
+        {
+          title: this.$t('POLICY.PKTS.TITLE_5'),
+          description: this.$t('POLICY.PKTS.DESCRIPTION_5'),
+        },
+      ];
+    },
+  },
+  methods: {
+    formattedText(text) {
+      return text.replace(/<br>/g, '\n');
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-.container {
-  height: 100vh;
+.formatted-text {
+  white-space: pre-wrap;
 }
 </style>
