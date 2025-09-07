699937e69f996f193841482a3f96246370de5dda
Vlad <vlad.herasymovychh@gmail.com>
2025-09-07 20:48:50 +0200

    feat: add GlassCard component with styling and props for enhanced UI

---
+++ src/components/UI/GlassCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="position-relative">
+    <span
+      v-if="title"
+      class="title"
+    >
+      {{ title }}
+    </span>
+    <div
+      class="glass-card"
+      :class="type"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    title: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'main',
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  .title {
+    position: absolute;
+    font-weight: 600;
+    font-size: 44px;
+    top: -36px;
+    left: 90px;
+    z-index: 100;
+  }
+</style>
