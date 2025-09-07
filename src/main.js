f3d2cae0acf65b2206f4b177df1875ec882848f6
Vlad <vlad.herasymovychh@gmail.com>
2025-09-07 20:18:59 +0200

    refactor: remove AOS dependency and related animation helper; integrate tsparticles for animations

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import i18n from './i18n';
 import router from './router/index';
 import store from './store/index';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import httpPlugin from './plugins/http';
+import Particles from '@tsparticles/vue3';
+import { loadFull } from 'tsparticles';
 
-AOS.init();
 const app = createApp(App);
-app.use(i18n);
 app.use(router);
 app.use(store);
 app.use(vuetify);
 app.use(httpPlugin);
+app.use(Particles, {
+  init: async (engine) => {
+    await loadFull(engine);
+  },
+});
 
 app.mount('#app');
