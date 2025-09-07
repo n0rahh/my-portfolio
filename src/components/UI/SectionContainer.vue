a3f93c1c52c49e1b883ba4bbf1a91a49cb567b3a
Vlad <vlad.herasymovychh@gmail.com>
2025-09-07 21:08:58 +0200

    feat: create SectionContainer component with template, script, and styles for layout

---
+++ src/components/UI/SectionContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container
+    :id="id"
+    class="container d-flex align-center"
+  >
+    <slot />
+  </v-container>
+</template>
+
+<script setup>
+  defineProps({
+    id: {
+      type: String,
+      default: '',
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    height: 100vh;
+  }
+</style>
