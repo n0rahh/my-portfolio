1b0f4ec14cf80a5a349d0ea824da4d0191e8c241
Vlad <vlad.herasymovychh@gmail.com>
2025-09-07 21:12:11 +0200

    feat: update Banner component layout with GlassCard and new avatar image

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -1,224 +1,86 @@
 <template>
-  <v-container
+  <SectionContainer
     id="banner"
-    class="mt-n16 container"
-    v-bind="aosAttribute('fade-right', 300, 800, 'ease-in-out', 'center', 50)"
+    class="mt-n16"
   >
     <v-row>
-      <v-col 
-        lg="10"
-        cols="12"
-        class="wrapper"
-      >
-        <div class="wrapper-static">
-          <span :class="title">
-            {{ $t('HOME_PAGE.HOME_BANNER.HELLO') }}
-          </span>
-          <span
-            v-if="$vuetify.display.mdAndDown && !$vuetify.display.xs"
-            :class="title"
-            class="ml-8"
-          >
-            {{ $t('HOME_PAGE.HOME_BANNER.IM') }}
-          </span>
-          <div
-            class="d-flex"
-            :class="{
-              'flex-column': $vuetify.display.smAndDown,
-            }"
-          >
-            <span
-              v-if="$vuetify.display.lgAndUp || $vuetify.display.xs"
-              :class="title"
-            >
-              {{ $t('HOME_PAGE.HOME_BANNER.IM') }}
-            </span>
-            <span>
-              <div
-                class="wrapper-animation"
-                :class="{
-                  'ml-16': $vuetify.display.lgAndUp, 
-                }"
-              >
-                <div class="first">
-                  <span
-                    class="l-3 subtitle"
-                    :class="{
-                      'h1': $vuetify.display.smAndUp, 
-                      'h2': $vuetify.display.xs, 
-                    }"
-                  >
-                    Vlad ✌️
-                  </span>
-                </div>
-                <div class="second">
-                  <span
-                    class="l-3 subtitle"
-                    :class="{
-                      'h1': $vuetify.display.smAndUp, 
-                      'h2': $vuetify.display.xs, 
-                    }"
-                  >
-                    Fullstack 💽
-                  </span>
-                </div>
-                <div class="third">
-                  <span
-                    class="l-3 subtitle"
-                    :class="{
-                      'h1': $vuetify.display.smAndUp, 
-                      'h2': $vuetify.display.xs, 
-                    }"
-                  >
-                    Web Developer💻
-                  </span>
-                </div>
-              </div>
-            </span>
+      <v-col cols="12">
+        <GlassCard>
+          <div class="d-flex flex-column">
+            <span class="h1"> Hello, </span>
+            <div class="d-flex flex-column">
+              <span class="h1"> I am Vlad </span>
+              <span class="mt-8 h4 subtitle">
+                Software Engineer | Web Developer | Tech Enthusiast
+              </span>
+            </div>
           </div>
-        </div>
+          <div class="photo-container">
+            <v-img
+              :src="avatar"
+              alt="My Photo"
+              class="my-photo"
+            />
+          </div>
+        </GlassCard>
       </v-col>
     </v-row>
-  </v-container>
+  </SectionContainer>
 </template>
 
-<script>
-import aosMixin from '@/helpers/animation';
-
-export default {
-  mixins: [aosMixin],
-  computed: {
-    title() {
-      return this.$vuetify.display.lgAndUp ? 'h0' : this.$vuetify.display.smAndUp ? 'h-unique' : 'h1';
-    },
-  },
-};
+<script setup>
+  import SectionContainer from '@/components/UI/SectionContainer.vue';
+  import GlassCard from '@/components/UI/GlassCard.vue';
+  import avatar from '@/assets/imgs/avatar.png';
 </script>
 
 <style lang="scss" scoped>
-@import '@/styles/colors.scss';
+  @use '@/styles/colors.scss' as *;
 
-.container {
-  height: 100vh;
-  display: flex;
-  align-items: center;
-}
+  .subtitle {
+    color: $text-inactive;
+  }
 
-.wrapper {
-  display: flex;
-  align-items: flex-end;
-  z-index: 1;
-  &-animation {
-    @media screen and (min-width: 650px) {
-      height: 5.31rem;
-    }
-    @media screen and (max-width: 649px) {
-      height: 3rem;
-    }
+  .photo-container {
+    width: 400px;
+    height: 400px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
     overflow: hidden;
-    margin-top: 2rem;
-    div>span {
-      @media screen and (min-width: 650px) {
-        height: 5.31rem;
-        margin-bottom: 5.31rem;
-      }
-      @media screen and (max-width: 649px) {
-        height: 3rem;
-        margin-bottom: 3rem;
-      }
-      display: inline-block;
-      padding-left: 0.7rem;
-      border-radius: 8px;
-    }
-    div {
-      &:first-child {
-        animation: text-animation 8s infinite;
-      }
-    }
-    .first {
-      span {
-        background-color: $banner-title-1;
-      }
-    }
-    .second {
-      span {
-        background-color: $banner-title-2;
-      }
-    }
-    .third {
-      span {
-        background-color: $banner-title-3;
-      }
-    }
-  }
-}
 
-@media screen and (min-width: 650px) {
-  @keyframes text-animation {
-    0% {
-      margin-top: 0;
+    background-color: $deep-blue-37;
+    backdrop-filter: blur(25px) saturate(150%);
+    -webkit-backdrop-filter: blur(25px) saturate(150%);
+    border: 1px solid $white-15;
+    box-shadow:
+      0 0 25px $cyan-dark,
+      inset 0 0 25px $blue-glow-02;
+
+    &:before {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 90%;
+      height: 90%;
+      border-radius: 50%;
+      background: radial-gradient(circle, $cyan-neon-3, transparent 60%);
+      filter: blur(50px);
+      opacity: 0.8;
+      z-index: 1;
     }
-    10% {
-      margin-top: 0;
-    }
-    20% {
-      margin-top: -10.62rem;
-    }
-    30% {
-      margin-top: -10.62rem;
-    }
-    40% {
-      margin-top: -21.24rem;
-    }
-    60% {
-      margin-top: -21.24rem;
-    }
-    70% {
-      margin-top: -10.62rem;
-    }
-    80% {
-      margin-top: -10.62rem;
-    }
-    90% {
-      margin-top: 0;
-    }  
-    100% {
-      margin-top: 0;
-    }
-  }
-}
-@media screen and (max-width: 649px) {
-  @keyframes text-animation {
-    0% {
-      margin-top: 0;
-    }
-    10% {
-      margin-top: 0;
-    }
-    20% {
-      margin-top: -6rem;
-    }
-    30% {
-      margin-top: -6rem;
-    }
-    40% {
-      margin-top: -12rem;
-    }
-    60% {
-      margin-top: -12rem;
-    }
-    70% {
-      margin-top: -6rem;
-    }
-    80% {
-      margin-top: -6rem;
-    }
-    90% {
-      margin-top: 0;
-    }  
-    100% {
-      margin-top: 0;
+
+    .my-photo {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      object-fit: cover;
+      box-shadow: 0 2px 10px $black-25;
+      z-index: 2;
     }
   }
-}
 </style>
