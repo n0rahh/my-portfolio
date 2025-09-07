16524ce874a54aeeecaffdb442ff6c49468e7760
Vlad <vlad.herasymovychh@gmail.com>
2025-09-07 20:27:40 +0200

    refactor: convert Home component to script setup syntax for improved performance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,26 +8,10 @@
   </v-container>
 </template>
 
-<script>
-import Banner from '@/components/home/Banner.vue';
-import About from '@/components/home/About.vue';
-import Projects from '@/components/home/Projects.vue';
-import Skills from '@/components/home/Skills.vue';
-import Contact from '@/components/home/Contact.vue';
-
-export default {
-  name: 'Home',
-  components: {
-    Banner,
-    Projects,
-    Skills,
-    Contact,
-    About,
-  },
-  data() {
-    return {
-            
-    };
-  },
-};
+<script setup>
+  import Banner from '@/components/home/Banner.vue';
+  import About from '@/components/home/About.vue';
+  import Projects from '@/components/home/Projects.vue';
+  import Skills from '@/components/home/Skills.vue';
+  import Contact from '@/components/home/Contact.vue';
 </script>
