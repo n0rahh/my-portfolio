ad5994ba34fec8a455d248f214824664a5e2342c
Vlad <vlad.herasymovychh@gmail.com>
2025-09-07 22:03:40 +0200

    feat: update Privacy Policy component with static content and refactor for clarity

--- src/views/Policy.vue
+++ src/views/Policy.vue
@@ -3,22 +3,25 @@
     class="container py-16"
     :class="{
       'px-6': $vuetify.display.mdAndDown && !$vuetify.display.xs,
-      'px-4': $vuetify.display.xs
+      'px-4': $vuetify.display.xs,
     }"
   >
     <v-row>
       <v-col cols="12">
-        <span class="h2">{{ $t('POLICY.TITLE') }}</span>
-        <br><br>
-        <span class="p2 mt-16">{{ $t('POLICY.DESCRIPTION') }}</span>
+        <span class="h2">Privacy policy</span>
+        <br /><br />
+        <span class="p2 mt-16">
+          This privacy policy outlines the types of personal information that is received and
+          collected by this website and how it is used.
+        </span>
         <div class="p2 mt-8">
           <p
             v-for="(pkt, index) in policyPkts"
             :key="index"
           >
             <span>{{ pkt.title }}</span>
-            <br>
-            <span class="formatted-text">{{ formattedText(pkt.description) }}</span>
+            <br />
+            <span class="formatted-text">{{ pkt.description }}</span>
           </p>
         </div>
       </v-col>
@@ -26,45 +29,38 @@
   </v-container>
 </template>
 
-<script>
-export default {
-  name: 'Policy',
-  computed: {
-    policyPkts() {
-      return [
-        {
-          title: this.$t('POLICY.PKTS.TITLE_1'),
-          description: this.$t('POLICY.PKTS.DESCRIPTION_1'),
-        },
-        {
-          title: this.$t('POLICY.PKTS.TITLE_2'),
-          description: this.$t('POLICY.PKTS.DESCRIPTION_2'),
-        },
-        {
-          title: this.$t('POLICY.PKTS.TITLE_3'),
-          description: this.$t('POLICY.PKTS.DESCRIPTION_3'),
-        },
-        {
-          title: this.$t('POLICY.PKTS.TITLE_4'),
-          description: this.$t('POLICY.PKTS.DESCRIPTION_4'),
-        },
-        {
-          title: this.$t('POLICY.PKTS.TITLE_5'),
-          description: this.$t('POLICY.PKTS.DESCRIPTION_5'),
-        },
-      ];
+<script setup>
+  const policyPkts = [
+    {
+      title: '1. Data Collection',
+      description:
+        "The website collects only the personal data provided voluntarily by the user through the contact form. This includes the user's name, email address, message, and optional file attachment. The website does not collect any other personal data or information.\n\n",
     },
-  },
-  methods: {
-    formattedText(text) {
-      return text.replace(/<br>/g, '\n');
+    {
+      title: '2. Data Usage',
+      description:
+        "The collected personal data is used solely for the purpose of responding to the user's inquiry or request. The website does not share, sell, or distribute any personal data to third parties. The website does not use personal data for marketing or advertising purposes.\n\n",
     },
-  },
-};
+    {
+      title: '3. Data Retention',
+      description:
+        "Personal data processed through the contact form will be stored for the period necessary to respond to the user's inquiry or request, as well as for any further contact related to that inquiry. After this time, the data will be deleted.\n\n",
+    },
+    {
+      title: '4. Recipients of personal data',
+      description:
+        'Personal data of users of the portfolio website are not disclosed to any other entities, except where required by law.\n\n',
+    },
+    {
+      title: '5. Cookies and Other Technologies',
+      description:
+        "The portfolio website does not use cookies or other tracking technologies. It only stores the locale value in localStorage to remember the user's selected language. This information is used solely to display the website in the selected language and is not shared with any other parties. Users can delete this value from localStorage at any time by setting the language to default or by clearing the localStorage completely.\n\n",
+    },
+  ];
 </script>
 
 <style lang="scss" scoped>
-.formatted-text {
-  white-space: pre-wrap;
-}
+  .formatted-text {
+    white-space: pre-wrap;
+  }
 </style>
