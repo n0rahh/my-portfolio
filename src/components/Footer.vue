b86be8703a8e568b50c8098c6b1b9f946147611d
Vlad <vlad.herasymovychh@gmail.com>
2025-09-07 22:02:55 +0200

    feat: update Footer component copyright year and refactor styles for improved clarity

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-footer class="footer mt-8">
+  <v-footer
+    id="footer"
+    class="footer mt-8"
+  >
     <v-row>
       <v-divider
         thickness="2"
@@ -9,17 +12,17 @@
         cols="12"
         class="py-0 mt-6 d-flex justify-center"
       >
-        <span class="p3">© Copyright 2024. Designed &amp; Built by Vlad Herasymovych</span>
+        <span class="p3"> © Copyright 2025. Designed &amp; Built by Vlad Herasymovych</span>
       </v-col>
     </v-row>
   </v-footer>
 </template>
 
 <style lang="scss" scoped>
-@import '@/styles/colors';
+  @use '@/styles/colors.scss' as *;
 
-.footer {
-  height: 100px;
-  background-color: $primary;
-}
+  .footer {
+    height: 100px !important;
+    background-color: transparent;
+  }
 </style>
